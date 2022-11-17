c4f319eae8ee60b345be5104c468ac5975752768
nurez305 <nurudeen.saliu@decagon.dev>
2022-11-17 12:07:36 +0100

    login design

--- src/Pages/login/Login.jsx
+++ src/Pages/login/Login.jsx
@@ -1,9 +1,37 @@
 import './login.scss'
 
 function Login() {
+
   return (
-    <div>Login</div>
-  )
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
+            alias totam numquam ipsa exercitationem dignissimos, error nam,
+            consequatur.
+          </p>
+          <span>Don't you have an account?</span>
+            <button>Register</button>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input
+              type="text"
+              placeholder="Username"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+            />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  ); 
 }
 
 export default Login
