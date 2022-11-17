7299b63864aea2a60a770b130d6134b04915c0ff
nurez305 <nurudeen.saliu@decagon.dev>
2022-11-17 13:57:47 +0100

    update

--- src/App.js
+++ src/App.js
@@ -5,9 +5,26 @@ import Register from "./Pages/register/Register";
 import {
   createBrowserRouter,
   RouterProvider,
-  //Outlet,
-  //Navigate,
+  Outlet,
+  Navigate,
 } from "react-router-dom";
+import Navbar from './components/navbar/Navbar';
+import LeftBar from './components/leftBar/LeftBar'
+import RightBar from './components/rightBar/RightBar'
+
+const Layout = () => {
+  return (
+    <div>
+       <Navbar />
+        <div style={{ display: "flex" }}>
+          <LeftBar />
+            <Outlet />
+          <RightBar />
+        </div>
+    </div>   
+    
+  );
+};
 
 function App() {
   const router = createBrowserRouter([
