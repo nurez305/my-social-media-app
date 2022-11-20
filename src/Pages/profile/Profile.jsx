26af89107f09acdcbd2603ebaff6169140c321b4
nurez305 <nurudeen.saliu@decagon.dev>
2022-11-20 16:51:33 +0100

    ui profile page

--- src/Pages/profile/Profile.jsx
+++ src/Pages/profile/Profile.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import "./profile.scss";
+import FacebookTwoToneIcon from "@mui/icons-material/FacebookTwoTone";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import PlaceIcon from "@mui/icons-material/Place";
+import LanguageIcon from "@mui/icons-material/Language";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { Link } from "@mui/material";
+import Posts from "../../components/posts/Posts"
 
 function Profile() {
   return (
-    <div>Profile</div>
+    <div className="profile">
+      <div className="images">
+        <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" className="cover"/>
+        <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" className="profilePic"/>
+      </div>
+      <div className="profileContainer">
+        <div className="uInfo">
+          <div className="left">
+            <Link><FacebookTwoToneIcon fontSize="large"/></Link>
+            <Link><InstagramIcon fontSize="large"/></Link>
+            <Link><TwitterIcon fontSize="large"/></Link>
+            <Link><LinkedInIcon fontSize="large"/></Link>
+             <Link><PinterestIcon fontSize="large"/></Link>
+          </div>
+          <div className="center">
+            <span>John Doe</span>
+            <div className="info">
+              <div className="item">
+                <PlaceIcon />
+                <span>Nigeria</span>
+              </div>
+              <div className="item">
+                <LanguageIcon />
+                <span>Yoruba</span>
+              </div>
+            </div>
+              <button>follow</button>
+          </div>
+          <div className="right">
+            <EmailOutlinedIcon />
+            <MoreVertIcon />
+          </div>
+        </div>
+        <Posts />
+      </div>
+    </div>
   )
 }
 
